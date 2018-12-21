207c5ee79a61e62d82e99ce0efcc85656c352601
Kiana Mohseni <k.t.mohseni@gmail.com>
2018-12-20 19:28:59 -0800

    added menu

--- learn_python/milestone_1/app.py
+++ learn_python/milestone_1/app.py
@@ -1,3 +1,7 @@
+"""
+Enter a to add a movie, l to list movies, f to find, q to quit.
+
+"""
 movies = [
     {
         'name': 'The alan',
@@ -7,17 +11,17 @@ movies = [
     {
         'name': 'The blue',
         'director': 'blue',
-        'year': '1994'
+        'year': '1997'
     },
     {
         'name': 'The tom',
         'director': 'tom',
-        'year': '1994'
+        'year': '1996'
     },
     {
         'name': 'The tom returns',
         'director': 'tom',
-        'year': '1994'
+        'year': '1995'
     },
     {
         'name': 'The peg',
@@ -25,22 +29,44 @@ movies = [
         'year': '1994'
     }
 ]
-director = movies[0]['director']
-print (movies)
-print (director)
-search_criteria = input (f'which criteria are you searching? ')
-print (search_criteria)
-search_key = input (f'which {search_criteria} are you looking for?')
-print (search_key)
-search_list = []
-print (search_list)
-search_list.append(search_key)
-print (search_list)
-results_list = []
-for i in range (len(movies)):
-    print(i)
-    print (movies[i])
-    if search_key == movies[i][search_criteria]:
-        results_list.append(movies[i])
 
-print (results_list)
+def add_movie():
+    name = input ('enter movie name: ')
+    director = input ('enter movie director:')
+    date = input ('enter movie date')
+    movies.append({'name':name, 'director':director,'date':date})
+    print (movies)
+    return
+
+
+
+def search_movie():
+    director = movies[0]['director']
+    print (movies)
+    print (director)
+    search_criteria = input (f'which criteria are you searching? ')
+    print (search_criteria)
+    search_key = input (f'which {search_criteria} are you looking for?')
+    print (search_key)
+    search_list = []
+    print (search_list)
+    search_list.append(search_key)
+    print (search_list)
+    results_list = []
+    for i in range (len(movies)):
+        if search_key == movies[i][search_criteria]:
+            results_list.append(movies[i])
+    print (results_list)
+    return
+
+while True:
+    user_input = input ('Enter a to add a movie, l to list movies, f to find, q to quit.')
+    print(user_input)
+    if user_input == 'a':
+        add_movie()
+    elif user_input == 'f':
+        search_movie()
+    elif user_input == 'l':
+        print('list function')
+    elif user_input == 'q':
+        Break
